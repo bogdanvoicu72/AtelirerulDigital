7eae080195ae0b35241e07f4ae82aa70f650e4f5
bogdanvoicu72 <bogdan.voicu99@e-uvt.ro>
2019-10-28 20:27:04 +0200

    Initial commit

---
+++ src/Lab1/Main.java
@@ -0,0 +1,14 @@
+package Lab1;
+import java.util.Scanner;
+public class Main {
+    public static void main (String []  args){
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        while(n!=0)
+        {
+            n=n-1;
+            System.out.println("HELLO VADIM!");
+        }
+            System.out.println("PRIVIET COLEGA");
+    }
+}
